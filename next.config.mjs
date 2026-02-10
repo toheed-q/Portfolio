5f821988486446829e12c57b62d215d9306dc02f
Toheed Qureshi <toheedhashmi78@gmail.com>
2026-02-10 16:52:58 +0500

    fix the version issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? `/venus-nextjs` : "";
-
 const nextConfig = {
-  output: "export",
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
-  },
-  trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
